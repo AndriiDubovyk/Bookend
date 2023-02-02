f633d98d2a801973a65e6a6a1509a00d36100f77
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-02-02 16:01:55 +0200

    Reset css

--- lib/src/main/java/com/artifex/mupdf/mini/CSSManager.java
+++ lib/src/main/java/com/artifex/mupdf/mini/CSSManager.java
@@ -9,21 +9,17 @@ public class CSSManager {
     public int fontSize = 10; // range 1-40
     public String textAlign = "justify";
 
-    private final float MIN_REAL_FONT_SIZE_EM = 5.0f;
-    private final String TABLE_FIX = "td {text-align: center !important;} td p{display: inherit !important;}";
+    private final static float MIN_REAL_FONT_SIZE_EM = 5.0f;
+    private final static String TABLE_FIX = "td {text-align: center !important;} td p{display: inherit !important;}";
 
     public CSSManager() {}
 
     public String getCSS() {
         float realFontSize = MIN_REAL_FONT_SIZE_EM + 0.5f * fontSize;
-        StringBuilder builder = new StringBuilder();
-        builder
-                .append("body {margin: 0 !important; padding: 0 !important; font-size: ")
-                .append(realFontSize).append("em !important; font-family: ")
-                .append(FONTS.get(font)).append(" !important;} p{text-align: ")
-                .append(textAlign).append(" !important;} p.center{text-align: center !important;}")
-                .append(TABLE_FIX);
-        return builder.toString();
+        String res = "body{margin: 0 !important; padding: 0 !important; font-size: "+realFontSize+"em !important; font-family: "+FONTS.get(font) + " !important;}" +
+                "p{text-align: "+textAlign+" !important;} " +
+                TABLE_FIX;
+        return res;
     }
 
     private static final HashMap<String, String> FONTS  = new HashMap<String, String>() {{
