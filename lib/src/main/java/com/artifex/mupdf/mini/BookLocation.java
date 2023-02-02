841562e2dfa54e27f5677685c5f6708d237e693f
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-02-02 18:07:11 +0200

    Add model for book location

---
+++ lib/src/main/java/com/artifex/mupdf/mini/BookLocation.java
@@ -0,0 +1,36 @@
+package com.artifex.mupdf.mini;
+
+import com.artifex.mupdf.fitz.Document;
+import com.artifex.mupdf.fitz.Location;
+
+public class BookLocation {
+    public final int chapter;
+    public final float chapterProgress;
+
+    public BookLocation(int chapter, float chapterProgress) {
+        this.chapter = chapter;
+        this.chapterProgress = chapterProgress;
+    }
+
+    public BookLocation(Document doc, Location loc) {
+        this.chapter = loc.chapter;
+        int currentChapterPages = doc.countPages(chapter);
+        this.chapterProgress = getReadProgress(loc.page, currentChapterPages);
+    }
+
+    public BookLocation(Document doc, int currentPage) {
+        this(doc, doc.locationFromPageNumber(currentPage));
+    }
+
+    public int toPage(Document doc) {
+        int currentChapterPages = doc.countPages(chapter);
+        int page = Math.round(chapterProgress * currentChapterPages);
+        if(page>=currentChapterPages)  page = currentChapterPages-1;
+        Location loc = new Location(this.chapter, page);
+        return doc.pageNumberFromLocation(loc);
+    }
+
+    public static float getReadProgress(int current, int count) {
+        return ((float) current) / (count - 1);
+    }
+}
